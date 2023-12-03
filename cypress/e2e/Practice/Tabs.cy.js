35c9030bfcb81867e0811ed101adbc385b4d6ef9
mindifire-shikha <107248924+mindfire-pushpit@users.noreply.github.com>
2023-12-03 17:24:35 +0530

    Update

---
+++ cypress/e2e/Practice/Tabs.cy.js
@@ -0,0 +1,27 @@
+describe('Handle Tabs',()=>{
+    it.skip('Apporach 1',()=>{
+        cy.visit('https://the-internet.herokuapp.com/windows')
+        cy.get('a[href="/windows/new"]').invoke('removeAttr', 'target').click();
+
+        cy.url().should('include', 'the-internet.herokuapp.com/windows/new')
+
+        cy.wait(5000);
+        cy.go('back')
+
+    })
+
+    it('Apporach 2',()=>{
+        cy.visit('https://the-internet.herokuapp.com/windows')
+        cy.get('.example > a').then((e)=>{
+            let url = e.prop('href');
+
+            cy.visit(url)
+        })
+
+        cy.url().should('include', 'the-internet.herokuapp.com/windows/new')
+
+        cy.wait(5000);
+        cy.go('back')
+
+    })
+})
