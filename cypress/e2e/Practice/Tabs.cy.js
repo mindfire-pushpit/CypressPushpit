064c2601e73a471f78fbeb4a83ee4e267c616c65
mindifire-shikha <107248924+mindfire-pushpit@users.noreply.github.com>
2023-12-03 17:26:54 +0530

    U

--- cypress/e2e/Practice/Tabs.cy.js
+++ cypress/e2e/Practice/Tabs.cy.js
@@ -10,11 +10,13 @@ describe('Handle Tabs',()=>{
 
     })
 
+
+    //  Apporach 2 having limitation that both Url have same domain.
+
     it('Apporach 2',()=>{
         cy.visit('https://the-internet.herokuapp.com/windows')
         cy.get('.example > a').then((e)=>{
             let url = e.prop('href');
-
             cy.visit(url)
         })
 
